19ca596f4a3f4630d7fcd9cb12e09b714a7320b0
Omar Faruqe <faruqe.cse@gmail.com>
2017-08-22 10:09:14 +0600

    Initial Commit with a test case and black geneFind method

---
+++ FindGene/FindGene.java
@@ -0,0 +1,15 @@
+public class FindGene
+{
+    public String findGeneSimple(String dna){
+        String result = "";
+        
+        return result;
+    }
+    
+    void testGene(){
+        String dna = "ACTGCTAATATGCTATAA";
+        String gene = findGeneSimple(dna);
+        System.out.println("DNA Strand: "+dna+"\nGene: "+gene);
+    }
+   
+}
