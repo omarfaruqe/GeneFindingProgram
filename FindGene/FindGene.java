0bf8e890586d3657a3804a2d78f9fba22005089a
Omar Faruqe <faruqe.cse@gmail.com>
2017-08-22 10:12:30 +0600

    updated gene finding method which finds a simple gene starting with ATG and ending with TAA

--- FindGene/FindGene.java
+++ FindGene/FindGene.java
@@ -2,7 +2,9 @@ public class FindGene
 {
     public String findGeneSimple(String dna){
         String result = "";
-        
+        int startIndex = dna.indexOf("ATG");
+        int stopIndex = dna.indexOf("TAA", startIndex + 3);
+        result = dna.substring(startIndex, stopIndex + 3);
         return result;
     }
     
